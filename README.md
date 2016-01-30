e41390b39c602f10486c4b8888dbe95b5df18e87
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2016-01-30 15:47:43 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # graph-rdfa-processor
-Green turtle GraphRdfaProcessor extracted and available as a commonJS module
+
+Green turtle GraphRdfaProcessor extracted and available as a
+standalone commonJS / ES2015 module.
+
+All credits are due to Alex Milowski for
+[green-turtle](https://github.com/alexmilowski/green-turtle).
+
+Aside from the adaptation to ES2015, some little changes were made to
+make it compatible with [jsdom](https://github.com/tmpvar/jsdom). Also
+proper Errors are now thrown instead of plain strings.
+
+For convenience, a high level API has been added:
+
+```
+import getRdfaGraph from graph-rdfa-processor;
+
+let graph = getRdfaGraph(document);
+console.log(graph.toString());
+```
+
+See the tests for a full example.
+
+## RDFa to JSON-LD
+
+see
+[jsonld-rdfa-parser](https://github.com/scienceai/jsonld-rdfa-parser)
+(using this module to extract the RDFa graph.
---
+++ test/test.js
@@ -0,0 +1,28 @@
+import { jsdom } from 'jsdom';
+import assert from 'assert';
+import getRDFaGraph from '../src';
+
+describe('getRDFaGraph', function() {
+  it('should getRDFaGraph from a document', function() {
+    let html =
+    `<div typeof="rdfs:Class" resource="http://schema.org/CreativeWork">
+        <span class="h" property="rdfs:label">CreativeWork</span>
+        <span property="rdfs:comment">The most generic kind of creative work, including books, movies, photographs, software programs, etc.</span>
+        <span>Subclass of: <a property="rdfs:subClassOf" href="http://schema.org/Thing">Thing</a></span>
+        <span>Source:  <a property="dc:source" href="http://www.w3.org/wiki/WebSchemas/SchemaDotOrgSources#source_rNews">rNews</a></span>
+     </div>`;
+
+    let { document } = jsdom(html, {url: 'file:///'}).defaultView.window;
+
+    let graph = getRDFaGraph(document);
+
+    let expected = `<http://schema.org/CreativeWork> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class>;
+ <http://www.w3.org/2000/01/rdf-schema#label> "CreativeWork";
+ <http://www.w3.org/2000/01/rdf-schema#comment> "The most generic kind of creative work, including books, movies, photographs, software programs, etc.";
+ <http://www.w3.org/2000/01/rdf-schema#subClassOf> <http://schema.org/Thing>;
+ <http://purl.org/dc/terms/source> <http://www.w3.org/wiki/WebSchemas/SchemaDotOrgSources#source_rNews> .
+`;
+
+   assert.equal(graph.toString(), expected);
+  });
+});
